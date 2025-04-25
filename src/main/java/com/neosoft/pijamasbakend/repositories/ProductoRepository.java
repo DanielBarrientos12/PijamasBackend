9f7cf4af0cca8c5d44755ac24c2fa770d2e5af49
DanielBarrientos12 <danielbarrientos15342@outlook.com>
2025-04-24 20:25:13 -0500

    se agrega para buscar por productos activos

--- src/main/java/com/neosoft/pijamasbakend/repositories/ProductoRepository.java
+++ src/main/java/com/neosoft/pijamasbakend/repositories/ProductoRepository.java
@@ -2,6 +2,11 @@ package com.neosoft.pijamasbakend.repositories;
 
 import com.neosoft.pijamasbakend.entities.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+    Page<Producto> findByActivoTrue(Pageable pageable);
+
 }
