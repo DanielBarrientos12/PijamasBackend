3bd8a14048441a64915102647bd155d15e082beb
DanielBarrientos12 <danielbarrientos15342@outlook.com>
2025-06-08 22:01:39 -0500

    clave no autogenerada

--- src/main/java/com/neosoft/pijamasbakend/security/JwtConfig.java
+++ src/main/java/com/neosoft/pijamasbakend/security/JwtConfig.java
@@ -1,6 +1,7 @@
 package com.neosoft.pijamasbakend.security;
 
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.context.annotation.Configuration;
 
 import javax.crypto.SecretKey;
@@ -8,7 +9,12 @@ import javax.crypto.SecretKey;
 @Configuration
 public class JwtConfig {
 
-    public static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+    private static final String SECRET_BASE64 =
+            "gS8l/4vQ7ZYJ2y6qN3EjVb++0O3P2K5Yb8H1Z2N9mMw=";
+
+    public static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(
+            Decoders.BASE64.decode(SECRET_BASE64)
+    );
     public static final long EXPIRATION_TIME_MS = 3600000;
 
 }
